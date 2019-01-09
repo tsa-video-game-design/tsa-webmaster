ff6cf074f98b828834fa603d1b32584d07ff242d
tsa-video-game-design <44676423+tsa-video-game-design@users.noreply.github.com>
2019-01-09 08:32:43 -0800

    Create tracking.js

---
+++ js/tracking.js
@@ -0,0 +1,22 @@
+//Things we want page tracking to do
+//Report page visited
+function getFileVisited() {
+  return window.location.pathname;
+}
+
+//Report coordinates of mouse
+function getMouseCoords(e) {
+  return [e.clientX,e.clientY];
+}
+
+//Report scrolling
+//Report browser type, window size
+
+//TRANSMIT TO SERVER
+
+//Events
+//On Mouse Move
+//On scroll
+//On page resize
+
+//Playback on home computer
