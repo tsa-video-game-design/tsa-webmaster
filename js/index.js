ca37d312d167c3da72784f3173d51ea4055e7165
tsa-video-game-design <44676423+tsa-video-game-design@users.noreply.github.com>
2019-03-01 08:20:34 -0800

    Made JS more concise

--- js/index.js
+++ js/index.js
@@ -6,9 +6,7 @@ $(document).ready(function(){
   if (window.location.pathname == "/tsa-webmaster/index.html" || window.location.pathname == "/tsa-webmaster/") {
      $(".c-m-f-submit").click(function(e) {
         $(".c-mailinglist").height($(".c-mailinglist").height()); //Locks the height so we don't get any dramatic flow shifts
-        $(".c-m-form").fadeOut();
-        $(".c-m-description").fadeOut();
-        $(".c-m-title").fadeOut(400,function() {
+        $(".c-mailinglist *").fadeOut(400,function() { //Fades out all children of c-mailinglist
           $(".c-m-title").text("Thanks for signing up! We'll send you periodic updates.");
               $(".c-m-title").fadeIn();
         });
