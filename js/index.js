784e2a02d836338aa916c9e521aaad0cc0f4aac2
tsa-video-game-design <44676423+tsa-video-game-design@users.noreply.github.com>
2019-03-01 08:17:39 -0800

    Locked height on registration form

--- js/index.js
+++ js/index.js
@@ -3,5 +3,15 @@ $(document).ready(function(){
   $(".h-hamburger").click(function(e) {
       $(".h-mobile-menu").slideToggle();
   });
-
+  if (window.location.pathname == "/tsa-webmaster/index.html" || window.location.pathname == "/tsa-webmaster/") {
+     $(".c-m-f-submit").click(function(e) {
+        $(".c-mailinglist").height($(".c-mailinglist").height()); //Locks the height so we don't get any dramatic flow shifts
+        $(".c-m-form").fadeOut();
+        $(".c-m-description").fadeOut();
+        $(".c-m-title").fadeOut(400,function() {
+          $(".c-m-title").text("Thanks for signing up! We'll send you periodic updates.");
+              $(".c-m-title").fadeIn();
+        });
+     });
+  }
 });
